382ec7f51d1756fb71e9a9ea337cf83d20d405a6
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2021-03-25 00:09:11 +0000

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="">
+        <h1 className="text-4xl">hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
